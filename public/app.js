f05959e8ecc2ae077deda19bcbdca1f8c20b8bd3
Johan Hobbs <johanhobbsnz@gmail.com>
2023-12-10 03:57:04 +0000

    Added new functions to JS file

--- public/app.js
+++ public/app.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const app = firebase.app();
+
+  const db = firebase.firestore();
+
+  const myPost = db.collection("posts".doc("firstpost"));
+
+  myPost.get().then((doc) => {
+    const data = doc.data();
+    document.write(data.title + `<br>`);
+    document.write(data.createdAt);
+  });
 });
 
 function googleLogin() {
