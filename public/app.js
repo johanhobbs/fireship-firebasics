dca1d8c034762a8b430379470e762e5731b2e9c2
johanhobbs <johanhobbsnz@gmail.com>
2023-12-14 17:13:37 +1300

    trying to get the document to read

--- public/app.js
+++ public/app.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  const postContainer = document.getElementById("post-container");
+
+  const {
+    initializeApp,
+    applicationDefault,
+    cert,
+  } = require("firebase-admin/app");
+  const {
+    getFirestore,
+    Timestamp,
+    FieldValue,
+    Filter,
+  } = require("firebase-admin/firestore");
+
   const app = firebase.app();
-
-  const db = firebase.firestore();
-
-  const myPost = db.collection("posts".doc("firstpost"));
 
   myPost.get().then((doc) => {
     const data = doc.data();
-    document.write(data.title + `<br>`);
-    document.write(data.createdAt);
+    postContainer.innerHTML = `<li><p>${data.title}</p> <p>${data.createdAt}</p></li>`;
+    console.log(`Post: ${myPost}, Title: ${data.title}`);
   });
 });
-
 function googleLogin() {
+  const accountContainer = document.getElementById("account-container");
   const provider = new firebase.auth.GoogleAuthProvider();
 
   firebase
@@ -20,7 +30,7 @@ function googleLogin() {
     .signInWithPopup(provider)
     .then((result) => {
       const user = result.user;
-      document.write(`Hello ${user.displayName}`);
+      accountContainer.innerHTML = `Hello ${user.displayName}`;
       console.log(user);
     });
 }
